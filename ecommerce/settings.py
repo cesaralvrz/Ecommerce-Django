6a78e760c39f462ab91097aff018057b8c741c4f
César Álvarez Llaneza <cesaralv97@gmail.com>
2020-06-04 13:48:40 +0200

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,16 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Agregamos esto
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
 
 MEDIA_URLS ='/images/'
-
-MEDIA_URL = "/images/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images') 
+# Cuando subamos una imagen se subirá gracias a este MEDIA ROOT
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
