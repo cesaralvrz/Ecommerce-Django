e487f595fb3055eff1cb840a7b1d9d0dfa7428f2
César Álvarez Llaneza <cesaralv97@gmail.com>
2020-06-04 14:10:43 +0200

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIA_URL ='/images/'
+MEDIA_URLS ='/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
